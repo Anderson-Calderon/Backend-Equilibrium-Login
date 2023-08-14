73efcd91a3b07249022dbbdfdbd24ecf7be66850
Anderson-Calderon <anderson.calderoncampos@gmail.com>
2023-08-13 20:54:46 -0500

    solucion 01

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -96,7 +96,7 @@ const obtenerUsuarios = async (req,res)=>{
 const autenticarUsuario = async (req,res)=>{
 
 	const {dni,password} = req.body;
-
+	console.log("ESTE ES EL NÚMERO DE DNI : ");
 
 	const existeUsuario = await Usuario.findOne({dni});
 
